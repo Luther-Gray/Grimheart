dd944f4183311772610f766a3c0ea83dd2bf2c6a
Luther-Gray <luther_the_developer@proton.me>
2026-03-19 16:40:05 -0400

    + Decoupled Player Camera and fixed sway. - Removed Yugen's Terrain Auth. Terrain3D has the better performance.   Around 3x better FPS.

--- Actors/Player/Player.gd
+++ Actors/Player/Player.gd
@@ -1,27 +1,9 @@
 extends CharacterBody3D
 
-@export var Setting : Settings
-@onready var Head: Node3D = $Clyde/Player/bHips/bTorso/bNeck
 const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-var isMouseCaptured = true
-
-func _ready() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-
-func _input(event: InputEvent) -> void:
-	if event.is_action_pressed("SYS_Escape"):  # Esc to toggle mouse
-		isMouseCaptured = !isMouseCaptured
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED if isMouseCaptured else Input.MOUSE_MODE_VISIBLE
-		get_viewport().set_input_as_handled()
-#/ Mouse Look
-	if event is InputEventMouseMotion and isMouseCaptured:
-# Left & Right Mouse Look
-		rotate_y(deg_to_rad(-event.relative.x * Setting.MouseSensitivity))
-# Up & Down Mouse Look
-		Head.rotate_x(deg_to_rad(-event.relative.y * Setting.MouseSensitivity))
-#Clamp how far up and down the player can look.
-		Head.rotation.x = clamp(Head.rotation.x, deg_to_rad(-85), deg_to_rad(85))
+const JUMP_VELOCITY = 20
+var InputDir : Vector2 = Vector2.ZERO
+var isMoving = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -29,26 +11,18 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("MV_Jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var InputDir := Input.get_vector("MV_Left", "MV_Right", "MV_Forward", "MV_Backward")
+	InputDir = Input.get_vector("MV_Left", "MV_Right", "MV_Forward", "MV_Backward")
 	var Direction := (transform.basis * Vector3(InputDir.x, 0, InputDir.y)).normalized()
 	if Direction:
 		velocity.x = Direction.x * SPEED
 		velocity.z = Direction.z * SPEED
+		isMoving = true
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
+		isMoving = false
 		
-			## Camera Sway
-	if InputDir.x > 0 :
-		Head.rotation.z = lerp_angle(Head.rotation.z, deg_to_rad(-Setting.SwayAmount), Setting.SwaySpeed * delta)
-	elif InputDir.x < 0 :
-		Head.rotation.z = lerp_angle(Head.rotation.z, deg_to_rad(Setting.SwayAmount),Setting.SwaySpeed * delta)
-	else:
-		Head.rotation.z = lerp_angle(Head.rotation.z, deg_to_rad(0), Setting.SwaySpeed * delta)
-
 	move_and_slide()
