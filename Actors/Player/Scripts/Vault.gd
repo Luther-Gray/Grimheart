37ba96b56501369e2e13aefd8d058dd8d3e72c40
Luther-Gray <luther_the_developer@proton.me>
2026-07-27 04:29:10 -0400

    Major Rework with Collision Layers + Vertical WallRun *kinda* working. + Vault moved to more deterministic System. No more Vaulting onto Rocks.

--- Actors/Player/Scripts/Vault.gd
+++ Actors/Player/Scripts/Vault.gd
@@ -4,26 +4,24 @@ extends Node3D
 @export var Source: CharacterBody3D
 @export var VaultSound : AudioStream
 @export var AudioPlayer : AudioStreamPlayer3D
-@onready var ClamberTarget: RayCast3D = $"../ClamberTargetRaycast"
 @onready var ClamberCast: ShapeCast3D = $"../ClamberShapecast"
 @onready var LedgeRay: RayCast3D = $"../LedgeCeilingRaycast"
 @onready var AnimManager: AnimationManager = $"../AnimCenter"
 @onready var DEV_Target: MeshInstance3D = $"../../DEV_Target"
 
+const VaultLayer : int = 2
+
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_pressed("MV_Jump"):
-		_detect_ledge()
+		_can_vault()
 
-func _detect_ledge():
-	if !ClamberCast.is_colliding() or LedgeRay.is_colliding() or !ClamberTarget.is_colliding():
+func _can_vault():
+	if !ClamberCast.is_colliding():
 		return
-	var TargetTopSurface = ClamberTarget.get_collision_point().y
-	var HeightDiff = TargetTopSurface - Source.global_position.y
-	
-	if HeightDiff < 1.2: #Waist Height - Vault
+	if ClamberCast.get_collision_mask_value(VaultLayer):
 		if Input.is_action_pressed("MV_Forward") and Source.is_on_floor() and Source.isSprinting:
 			_vault()
-	else: # Too Tall - Ignore
+	else:
 		return
 
 #// Vault
@@ -37,24 +35,16 @@ func _vault():
 	get_tree().create_timer(1.0).timeout.connect(func(): Source.MoveSpeed -= 4.5)
 	
 	Source.isVaulting = true
-	var VaultTarget = ClamberTarget.get_collision_point().y
 	var VaultDirection = -Source.global_transform.basis.z
 	var VaultBoost : float = 1.8
-	
-	# Disable Collision
-	var CastObject = ClamberCast.get_collider(0)
-	if CastObject:
-		Source.add_collision_exception_with(CastObject)
 	# Boost Vault
 	var VaultTween = get_tree().create_tween()
 	VaultTween.set_parallel(true)
-	VaultTween.tween_property(Source, "global_position:y", VaultTarget, 0.01)
+	VaultTween.tween_property(Source, "global_position:y", Source.global_position.y + 1.0, 0.01)
 	VaultTween.tween_property(Source, "global_position:x", Source.global_position.x + VaultDirection.x * VaultBoost, 0.5)
 	VaultTween.tween_property(Source, "global_position:z", Source.global_position.z + VaultDirection.z * VaultBoost, 0.3)
 	
-	VaultTween.tween_callback(func():
-		if CastObject:
-			Source.remove_collision_exception_with(CastObject)
+	VaultTween.tween_callback(func(): # When the Tween Ends ->
 		AnimManager.AnimOverride = false
 		Source.isVaulting = false
 		
--- Actors/Player/Scripts/WallRun.gd
+++ Actors/Player/Scripts/WallRun.gd
@@ -2,39 +2,59 @@
 extends Node3D
 
 @export var Source: CharacterBody3D
+@export var CameraPivot: Node3D
+@export var WallRunSound: AudioStream
+@export var TicTacSound: AudioStream
+@export var AudioPlayer: AudioStreamPlayer3D
+
+@export_category("Wall Run Params")
+var WallRunDuration: float = 1.5
+var VerticalRunDuration: float = 0.5
+var VerticalScrambleSpeed: float = 6.0
 
 @onready var WallL: RayCast3D = $"../WallRayL"
 @onready var WallR: RayCast3D = $"../WallRayR"
 @onready var ClamberShapeCast: ShapeCast3D = $"../ClamberShapecast"
-
-var WallRunTimer : float = 0.0
-var WallRunDuration : float = 0.3 # Replace this with stamina?
+@onready var AnimManager: AnimationManager = $"../AnimCenter"
+@onready var Jump: Node3D = $"../Jump"
+var WallRunTimer: float = 0.0
 
 func _physics_process(delta: float) -> void:
-	if Source.is_on_floor():
-		WallRunTimer = WallRunDuration
-	elif Source.isWallRunning and !Source.is_on_floor():
+	if Source.isWallRunning:
 		WallRunTimer -= delta
-	if Source.isFalling and Input.is_action_pressed("MV_Jump"):
-		if WallR.is_colliding():
-			pass
-		elif WallL.is_colliding():
-			pass
-	elif Input.is_action_pressed("MV_Jump") and Source.isSprinting:
-		if ClamberShapeCast.is_colliding():
-			pass
-	if WallRunTimer <= 0.0:
-		_detatch_wall()
+		if WallRunTimer <= 0.0:
+			_detatch_wall()
+			return
+			
+	if Input.is_action_just_pressed("MV_Jump") and Source.isSprinting and ClamberShapeCast.get_collider(0):
+		#_wall_run_v()
+		pass
 
 #// Physics - Wallruns are seperated for extra logic. Heavy 1 Handed Weapons. Broken Arms. Etc.
-func _wall_run_v(): #// Upwards scramble
+func _wall_run_v() -> void: #// Upwards scramble
+	if not ClamberShapeCast.is_colliding(): # Can you wallrun?
+		return
+	var WallNormal = ClamberShapeCast.get_collision_normal(0)
+	Source.velocity.y = VerticalScrambleSpeed * clampf(WallRunTimer / VerticalRunDuration, 0.0, 1.0)
+	Source.velocity.x = -WallNormal.x * 1.5
+	Source.velocity.z = -WallNormal.z * 1.5
 	Source.isWallRunning = true
-	Source.velocity += Vector3(0,1,0)
-func _wall_run_r(): #// Wallrun as long as stamina allows | RIGHT
-	Source.isWallRunning = true
-func _wall_run_l(): #// Wallrun as long as stamina allows | LEFT
-	Source.isWallRunning = true
-func _tic_tac(): #// Jump from wall
+	WallRunTimer = VerticalRunDuration
+	if Jump:
+		Jump.WasAirborne = true
+	if AnimManager:
+		AnimManager._override_travel("aWallRunUp")
+	else:
+		return
+
+func _wall_run_r() -> void: #// Wallrun as long as stamina allows | RIGHT
 	pass
-func _detatch_wall(): #// Lets go of wall
+
+func _wall_run_l() -> void: #// Wallrun as long as stamina allows | LEFT
+	pass
+
+func _tic_tac() -> void: #// Jump from wall
+	pass
+
+func _detatch_wall() -> void: #// Lets go of wall
 	Source.isWallRunning = false
